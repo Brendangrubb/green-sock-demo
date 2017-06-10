3f63aa08651110966825cd075f7a3801f997e303
Brendan Grubb <brendangrubb@gmail.com>
2017-06-09 19:24:30 -0700

    add more animations

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@ function backwardsize(input) { // Reverses Input String
   return joinedInput;
 };
 
+function testFunction() {
+  alert("Green Sock Rules!");
+}
+
 // User Interface Logic
 
 $(document).ready(function() {
@@ -30,15 +34,45 @@ $(document).ready(function() {
   // Green Sock
   var logoOne = document.getElementById("logo-id");
   var logoTwo = document.getElementById("logo-two-id");
-  TweenMax.to(logoOne, 6, {
-    left:600,
-    backgroundColor:"#c66a1c",
-    border:"5px solid black",
-    borderRadius:15});
-  TweenMax.to(logoTwo, 6, {
-    right:600,
-    backgroundColor: "#1c78c6",
-    border:"5px solid black",
-    borderRadius:15});
 
+  // ANIMATION logos cross left to right, add background-color
+  // TweenMax.to(logoOne, 6, {
+  //   left:600,
+  //   backgroundColor:"#c66a1c",
+  //   border:"5px solid black",
+  //   borderRadius:15});
+  // TweenMax.to(logoTwo, 6, {
+  //   right:600,
+  //   backgroundColor: "#1c78c6",
+  //   border:"5px solid black",
+  //   borderRadius:15}
+
+  // ANIMATION fly away/fly in (.to/.from)
+  // TweenMax.from(logoOne, 3, {x:600, rotation:360, scale:.05})
+  // TweenMax.from(logoTwo, 3, {x:-600, rotation:-360, scale:.05})
+
+  // ANIMATION bounce in
+  TweenMax.from(logoOne, 1.5, {
+    opacity:0,
+    scale:0,
+    ease:Bounce.easeOut
+  });
+  TweenMax.from(logoTwo, 2, {
+    opacity:0,
+    scale:0,
+    ease:Elastic.easeOut,
+    delay:1,
+    onComplete: testFunction
+  });
+
+  TweenMax.staggerFrom(".box", 1, {
+    opacity:0,
+    y:200,
+    delay:0.5,
+    rotation: 360,
+    scale:2,
+    ease: Circ.easeOut,
+    delay:1,
+  },
+  0.2);
 });
