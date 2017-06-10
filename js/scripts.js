c9c9bb9a77953f7db81cbaae49f3637229ea4d97
Brendan Grubb <brendangrubb@gmail.com>
2017-06-09 18:09:55 -0700

    add simple gs animations, second image

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 // Business Logic
 
 function backwardsize(input) { // Reverses Input String
@@ -19,7 +22,23 @@ $(document).ready(function() {
   $("#backwards-name-form").submit(function(event) {
     var input = $('#input').val();
     var output = backwardsize(input);
+    $(".output-intro").addClass("visible");
     $("#output").text(output);
     event.preventDefault();
   });
+
+  // Green Sock
+  var logoOne = document.getElementById("logo-id");
+  var logoTwo = document.getElementById("logo-two-id");
+  TweenMax.to(logoOne, 6, {
+    left:600,
+    backgroundColor:"#c66a1c",
+    border:"5px solid black",
+    borderRadius:15});
+  TweenMax.to(logoTwo, 6, {
+    right:600,
+    backgroundColor: "#1c78c6",
+    border:"5px solid black",
+    borderRadius:15});
+
 });
